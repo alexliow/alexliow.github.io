b32d4c22854978116dcd2b0c37a5aa8b073b2da2
Alex Liow <gll34@cornell.edu>
2016-03-07 12:00:54 -0500

    Testing ajax

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,13 +1,14 @@
 <script type="text/javascript">
 //auto expand textarea
 function adjust_textarea(h) {
+	window.alert("HELLO!");
     h.style.height = "20px";
     h.style.height = (h.scrollHeight)+"px";
 }
 
 $(function() {
     $('form').submit(function() {
-    	window.alert("HELLO!");
+
         $.ajax({
             type: 'POST',
             url: 'http://formspree.io/liow.alex@gmail.com',
